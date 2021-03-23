e015defb8f8ba46c73fc801bc57dbb837f79ad43
DESKTOP-J9QK6DP\Jac <lu7766lu7766@gmail.com>
2021-03-23 16:16:19 +0800

    tt

--- src/pages/Computed.tsx
+++ src/pages/Computed.tsx
@@ -3,7 +3,7 @@ import { useState, useMemo } from "react";
 export default function Computed(): JSX.Element {
   const [count, setCount] = useState(0);
   const data = {
-    double: useMemo(() => count * 2, [count]),
+    double2: useMemo(() => count * 2, [count]),
     get double4() {
       return count * 4;
     },
@@ -15,7 +15,7 @@ export default function Computed(): JSX.Element {
         +1
       </button>
       <div>Count: {count}</div>
-      <div>Double*2: {data.double}</div>
+      <div>Double*2: {data.double2}</div>
       <div>Double*4: {data.double4}</div>
     </div>
   );
